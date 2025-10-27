605402dac2dba5553765e6af0b70bc61ac23d56c
HaoNYC <haoxuanz6@nycstudents.net>
2025-10-27 13:26:10 +0000

    up

---
+++ Lesson-15-Activity/Main.java
@@ -0,0 +1,44 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+    print(spaces(10) + "DONE");
+    print(harmonic(5));
+    print(fib(6));
+  }
+
+  String spaces(int N) {
+    if (N < 0) {
+        return "";
+    }
+    return " ".repeat(N);
+  }
+  
+  double harmonic(int N) {
+    double sum = 0.0;
+    for (int i = 1; i <= N; i++) {
+        sum += 1.0 / i;
+    }
+    return sum;
+  }
+  // C3
+  int fib(int N) {
+    int t1 = 0;
+    int t2 = 1;
+    System.out.println("First " + N + " Fibonacci numbers:");
+    for (int i = 1; i <= N; ++i) {
+        System.out.print(t1 + ", ");
+        int sum = t1 + t2;
+        t1 = t2;
+        t2 = sum;
+    }
+    return t1 ;
+  }
+  
+}
