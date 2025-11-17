7cbc5168b4256485dc1077bbe8b2d0b06067271a
HaoNYC <haoxuanz6@nycstudents.net>
2025-11-17 03:26:09 +0000

    up

--- Lesson-21-Activity/Main.java
+++ Lesson-21-Activity/Main.java
@@ -8,15 +8,34 @@ class Main {
   void printt(Object o){System.out.print(o);}
 
   void init(){
-    
+    allowance();
+    GCF(5, 10);
   }
-
-
 
   
   int randInt(int lower, int upper){
     int range = upper - lower;
     return (int)(Math.random()*range+lower);
+  }
+
+  int allowance(){
+    double target = 5.00;
+    double penny = 0.01;
+    int days = 1;
+    while(penny < target){
+      penny *= 2;
+      days++;
+    }
+    return days;
+  }
+
+  int GCF(int a, int b){
+    while(b != 0){
+      int temp = b;
+      b = a % b;
+      a = temp;
+    }
+    return a;
   }
 
 }
