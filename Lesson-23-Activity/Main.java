b6cec3286d4067269f0f18d98eb4400d5b40dece
HaoNYC <haoxuanz6@nycstudents.net>
2025-11-23 22:58:57 +0000

    up

--- Lesson-23-Activity/Main.java
+++ Lesson-23-Activity/Main.java
@@ -8,18 +8,50 @@ class Main {
   void printt(Object o){System.out.print(o);}
 
   void init(){
-    double[] prices = {10,15,22,54,32,34,65,87,45,100,23,99,95,87,56,83};
-    print(total(prices));
-  }
-  String total(double[]  prices){
-    double sum = 0;
-    for(int i=0;i<prices.length;i++){
-      sum += prices[i] *1.08525;
-    }
-    return "sum is " + sum;
+    double[] prices = {10.5, 20.0, 5.75};
+    print("Total with tax: " + total(prices));
+    int[] nums = {3, 7, 11, 15, 20};
+    print("Is 11 in array? " + isin(nums, 11));
+    int[] myLotto = {3, 7, 11, 15, 20, 25};
+    int[] drawnLotto = {7, 10, 11, 15, 30, 40};
+    print("Matched: " + checkLotto(myLotto, drawnLotto));
+    int[] arr = {12, 45, 7, 89, 23};
+    print("Biggest number: " + biggest(arr));
   }
 
-  boolean 
+  double total(double[] prices){
+    double sum = 0;
+    for(int i=0; i<prices.length; i++){
+      sum += prices[i];
+    }
+    double taxRate = 0.08525;
+    return sum + sum*taxRate;
+  }
+
+  boolean isin(int[] arr, int num){
+    for(int i=0; i<arr.length; i++){
+      if(arr[i] == num) 
+        return true;
+    }
+    return false;
+  }
+
+  int checkLotto(int[] picks, int[] drawn){
+    int count = 0;
+    for(int i=0; i<picks.length; i++){
+      if(isin(drawn, picks[i])) count++;
+    }
+    return count;
+  }
+
+  int biggest(int[] arr){
+    int max = arr[0];
+    for(int i=1; i<arr.length; i++){
+      if(arr[i] > max) 
+        max = arr[i];
+    }
+    return max;
+  }
 
   // Use these functions below for the activity
 
